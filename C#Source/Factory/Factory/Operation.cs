80773430e62422bf0a1336311c404f3513a8d769
jiongjiong <houchaoneng@126.com>
2013-01-31 15:36:38 +0800

    工厂设计模式

---
+++ C#Source/Factory/Factory/Operation.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Factory
+{
+    public class Operation
+    {
+        private double numA, numB;
+        public double NUM_A
+        {
+            get { return numA; }
+            set { numA = value; }
+        }
+        public double NUM_B
+        {
+            get { return numB; }
+            set { numB = value; }
+        }
+        public virtual double getResult()
+        {
+            double result = 0;
+            return result;
+        }
+
+    }
+
+    class OperationAdd : Operation
+    { 
+        public override double getResult()
+        {
+            double result = 0;
+            result = NUM_A + NUM_B;
+            return result;
+        }
+    }
+    class OperationSub : Operation
+    {
+        public override double getResult()
+        {
+            return NUM_A - NUM_B;
+        }
+    }
+
+    class OperationMul : Operation
+    {
+        public override double getResult()
+        {
+            return NUM_A * NUM_B;
+        }
+    }
+
+    class OperationDev : Operation
+    {
+        public override double getResult()
+        {
+            double result = 0;
+            if (NUM_B == 0)
+            {
+                throw new Exception("除数不能够为 0");
+            }
+            else
+            {
+                result = NUM_A / NUM_B;
+            }
+            return result;
+        }
+    }
+
+    public class OperitionFactory
+    {
+        public static Operation createOperiton(String operation)
+        {
+            Operation oper = null;
+
+            switch(operation)
+            {
+                case "+":
+                    {
+                        oper = new OperationAdd(); 
+                        break;
+                    }
+                case "-":
+                    {
+                        oper = new OperationSub();
+                        break;
+                    }
+                case "*":
+                    {
+                        oper = new OperationMul();
+                        break;
+                    }
+                case "/":
+                    {
+                        oper = new OperationDev();
+                        break;
+                    }
+                default:
+                    {
+                        throw new Exception("输入操作符出错");
+                    }
+            }
+            return oper;
+        }
+    }
+}
---
+++ C#Source/Factory/Factory/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {             
+                try
+                {
+
+                    Console.WriteLine("请输入数字A");
+                    String strA = Console.ReadLine();
+                    Console.WriteLine("请输入数字B");
+                    String strB = Console.ReadLine();
+                    Console.WriteLine("请输入操作命令 如 + - * %");
+                    String strCmd = Console.ReadLine();
+
+                    Operation oper;
+                    oper = OperitionFactory.createOperiton(strCmd);
+
+                    oper.NUM_A = Convert.ToDouble(strA);
+                    oper.NUM_B = Convert.ToDouble(strB);
+                    Console.WriteLine(Convert.ToString(oper.getResult()));               
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("请检查输入是否有错" + ex.Message);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
