e1b60c3be810829e72aef26e8916498e6f85ba6d
jiongjiong <houchaoneng@126.com>
2013-02-02 16:52:11 +0800

    添加策略模式

---
+++ C#Source/strategy/strategy/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.WriteLine("请输入金钱");
+                string money = Console.ReadLine();
+                Console.WriteLine("请输入方式");
+                string strCmd = Console.ReadLine();
+                cashContext cc = new cashContext(strCmd);
+                double totalPrace = 0;
+
+                totalPrace = cc.GetResult(Convert.ToDouble(money));
+                Console.WriteLine("结果为 ："+ totalPrace.ToString());
+            }
+
+        }
+    }
+}
---
+++ C#Source/strategy/strategy/Strategy.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace strategy
+{
+    abstract class cashSuper
+    {
+        public abstract double acceptCrash(double money);
+        
+    };
+
+    class normalCrash : cashSuper
+    { 
+        public  override double acceptCrash(double money)
+        {
+            return money;
+        }
+    };
+
+    class CashRebate : cashSuper
+    {
+        private double moneyRebate = 1d;
+
+        public CashRebate(String strMoneyRebat)
+        {
+            this.moneyRebate = double.Parse(strMoneyRebat);
+        }
+
+        public override double acceptCrash(double money)
+        {
+            return money * moneyRebate;
+        }
+    };
+
+    class CashReturn : cashSuper
+    {
+        private double moneyCondition = 0;
+        private double moneyReturn = 0;
+
+        public CashReturn(string mCondition, string mReturn)
+        {
+            this.moneyCondition = double.Parse(mCondition);
+            this.moneyReturn = double.Parse(mReturn);
+        }
+        public override double acceptCrash(double money)
+        {
+            //)throw new NotImplementedException();
+            if (money > moneyCondition)
+            {
+                return money - moneyReturn;
+            }
+            return money;
+        }
+    };
+
+
+    class cashContext
+    {
+        private cashSuper cs;
+
+        public cashContext(string strCmd)
+        {
+            switch (strCmd)
+            {
+                case "1":
+                    {
+                        cs = new normalCrash();
+                        break;
+                    }
+                case "2":
+                    {
+                        cs = new CashRebate("0.8");
+                        break;
+                    }
+                case "3":
+                    {
+                        cs = new CashReturn("300", "100");
+                        break;
+                    }
+                default:
+                    {
+                        cs = null;
+                        break;
+                    }
+            }
+        }
+
+        public double GetResult(double money)
+        {
+            if (cs != null)
+            {
+                return cs.acceptCrash(money);
+            }
+            else
+            {
+                return 0;
+             }
+        }
+    };
+}
